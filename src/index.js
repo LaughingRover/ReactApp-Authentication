b422d713d4701dcc37680aac2eaf47e32b7650d4
LaughingRover <odohofreh.joseph.i@gmail.com>
2021-03-24 08:30:07 +0000

    master branch initialization

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/LoginHeader'
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
+import Footer from './components/Footer'
 
+// test
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		{/* <App /> */}
+
+		<Header />
+		<SignIn />
+		<SignUp />
+		<Footer />
+
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
